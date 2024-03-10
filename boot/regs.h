dfce2d1acab18bb5a1c840833275c94c72abf6a3
NukedOne <x@nuked.one>
2024-03-10 12:16:47 +0100

    wip

--- boot/regs.h
+++ boot/regs.h
@@ -2,21 +2,8 @@
 #define BOOT_REGS_H
 
 struct registers {
-    unsigned int ds;
-    unsigned int ebp;
-    unsigned int esi;
-    unsigned int edi;
-    unsigned int edx;
-    unsigned int ecx;
-    unsigned int ebx;
-    unsigned int eax;
     unsigned int int_no;
     unsigned int int_err_code;
-    unsigned int eip;
-    unsigned int cs;
-    unsigned int eflags;
-    unsigned int esp;
-    unsigned int ss;  
 };
 
 #endif
